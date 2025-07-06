d1af01f1a434eaddcf17b83d37b97450e946c800
NagaprasadVr <nagaprasadvr246@gmail.com>
2025-07-06 08:08:36 +0530

    feat: add scaffolding for token-2022 extensions

--- programs/token-2022/src/lib.rs
+++ programs/token-2022/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+pub mod extensions;
 pub mod instructions;
 pub mod state;
 
@@ -14,4 +15,24 @@ fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
     for (d, s) in destination.iter_mut().zip(source.iter()) {
         d.write(*s);
     }
+}
+
+/// Deserialize a type from a byte array.
+///
+/// # Safety
+///
+/// This function is unsafe because it transmutes the input data to the output type.
+pub unsafe fn from_bytes<T: Clone + Copy>(data: &[u8]) -> T {
+    assert_eq!(data.len(), core::mem::size_of::<T>());
+    *(data.as_ptr() as *const T)
+}
+
+/// Deserialize a type from a byte array into a reference.
+///
+/// # Safety
+///
+/// This function is unsafe because it transmutes the input data to the output type.
+pub unsafe fn from_bytes_ref<T: Clone + Copy>(data: &[u8]) -> &T {
+    assert_eq!(data.len(), core::mem::size_of::<T>());
+    &*(data.as_ptr() as *const T)
 }
